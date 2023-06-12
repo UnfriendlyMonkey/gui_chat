a71f9e1441250f5766c163f104ca2da2c2392dee
UnfriendlyMonkey <andreysereda@ya.ru>
2023-06-12 23:34:49 +0300

    Show history in the GUI (Lesson 5, Step 6)

--- chat_client.py
+++ chat_client.py
@@ -1,9 +1,9 @@
 import asyncio
 import aiofiles
 import datetime
-import configargparse
+# import configargparse
 import logging
-from argparse import Namespace
+# from argparse import Namespace
 
 from utils import get_asyncio_connection
 
@@ -21,65 +21,70 @@ def formatted_time() -> str:
 async def listen_tcp_chat(
     host: str,
     port: int,
-    queue: asyncio.Queue,
-    history_file: str
-):
+    messages_queue: asyncio.Queue,
+    save_queue: asyncio.Queue,
+) -> None:
     async with get_asyncio_connection(host=host, port=port) as connection:
         reader, _ = connection
-        async with aiofiles.open(history_file, mode='a') as log_file:
-            await log_file.write(f'{formatted_time()} Connection is set\n')
-            logger.debug(
-                f'Client is on at {formatted_time()} on {host}, {port}'
-            )
-            while True:
-                try:
-                    data = await reader.readline()
-                    message = data.decode()
-                    if message:
-                        # print(message, end='')
-                        queue.put_nowait(message)
-                        await log_file.write(f'{formatted_time()} {message}')
-                except KeyboardInterrupt:
-                    print('\nGoodbye!')
-                    logger.debug('Client was closed by KeyboardInterrupt')
-                    break
+        save_queue.put_nowait('Connection is set\n')
+        logger.debug(
+            f'Client is on at {formatted_time()} on {host}, {port}'
+        )
+        while True:
+            try:
+                data = await reader.readline()
+                message = data.decode()
+                if message:
+                    messages_queue.put_nowait(message.rstrip())
+                    save_queue.put_nowait(message)
+            except KeyboardInterrupt:
+                print('\nGoodbye!')
+                logger.debug('Client was closed by KeyboardInterrupt')
+                break
 
 
-def parse_arguments() -> Namespace:
-    parser = configargparse.ArgParser(
-        default_config_files=['config.txt'],
-        description='''Local chat client.
-        Print messages to stdout and save them to file'''
-    )
-    parser.add(
-        '-s',
-        '--host',
-        nargs='?',
-        help='host site to be connected to'
-    )
-    parser.add(
-        '-p',
-        '--port',
-        type=int,
-        nargs='?',
-        help='host port to be connected to'
-    )
-    parser.add(
-        '-y',
-        '--history',
-        nargs='?',
-        help='file to write chat history to'
-    )
-    args = parser.parse_known_args()
-
-    return args
+async def save_messages(history_file: str, save_queue: asyncio.Queue) -> None:
+    async with aiofiles.open(history_file, mode='a') as log_file:
+        while True:
+            message = await save_queue.get()
+            await log_file.write(f'{formatted_time()} {message}')
 
 
-def main():
-    args = parse_arguments()[0]
-    host, port, history_file = args.host, args.port, args.history
-    asyncio.run(listen_tcp_chat(host, port, history_file))
+# def parse_arguments() -> Namespace:
+#     parser = configargparse.ArgParser(
+#         default_config_files=['config.txt'],
+#         description='''Local chat client.
+#         Print messages to stdout and save them to file'''
+#     )
+#     parser.add(
+#         '-s',
+#         '--host',
+#         nargs='?',
+#         help='host site to be connected to'
+#     )
+#     parser.add(
+#         '-p',
+#         '--port',
+#         type=int,
+#         nargs='?',
+#         help='host port to be connected to'
+#     )
+#     parser.add(
+#         '-y',
+#         '--history',
+#         nargs='?',
+#         help='file to write chat history to'
+#     )
+#     args = parser.parse_known_args()
+
+#     return args
 
 
-if __name__ == '__main__':
-    main()
+# def main():
+#     args = parse_arguments()[0]
+#     host, port, history_file = args.host, args.port, args.history
+#     asyncio.run(listen_tcp_chat(host, port, history_file))
+
+
+# if __name__ == '__main__':
+#     main()
--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import asyncio
 import gui
 import configargparse
 from argparse import Namespace
-from chat_client import listen_tcp_chat
+from chat_client import listen_tcp_chat, save_messages
 
 
 messages_queue = asyncio.Queue()
 sending_queue = asyncio.Queue()
 status_updates_queue = asyncio.Queue()
+save_queue = asyncio.Queue()
 
 
 def parse_arguments() -> Namespace:
@@ -43,8 +44,13 @@ def parse_arguments() -> Namespace:
 async def main():
     args = parse_arguments()[0]
     host, port, history_file = args.host, args.port, args.history
+    with open(history_file, 'r') as file:
+        # load history to the GUI
+        while line := file.readline():
+            messages_queue.put_nowait(line.rstrip())
     await asyncio.gather(
-        listen_tcp_chat(host, port, messages_queue, history_file),
+        listen_tcp_chat(host, port, messages_queue, save_queue),
+        save_messages(history_file, save_queue),
         gui.draw(
             messages_queue,
             sending_queue,
